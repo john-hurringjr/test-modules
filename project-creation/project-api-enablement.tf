6acef0a1c982caa82d7410e8dfacdc8c7d0227fc
John Hurring Jr <jhurring@google.com>
2019-12-02 12:01:19 -0500

    enabled all apis again

--- project-creation/project-api-enablement.tf
+++ project-creation/project-api-enablement.tf
@@ -17,12 +17,12 @@
   APIs
  *****************************************/
 
-//resource "google_project_service" "enable_pubsub_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "pubsub.googleapis.com"
-//  disable_on_destroy = false
-//}
+resource "google_project_service" "enable_pubsub_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "pubsub.googleapis.com"
+  disable_on_destroy = false
+}
 
 resource "google_project_service" "enable_compute_api" {
   depends_on = [google_project.project]
@@ -31,94 +31,94 @@ resource "google_project_service" "enable_compute_api" {
   disable_on_destroy = false
 }
 
-//resource "google_project_service" "enable_bigtable_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "bigtable.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_bigquery_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "bigquery.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_logging_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "logging.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_cloudkms_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "cloudkms.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_storage_component_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "storage-component.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_storage_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "storage-api.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_storage_transfer_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "storagetransfer.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_dataflow_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "dataflow.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_cloudbuild_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "cloudbuild.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_cloudfunctions_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "cloudfunctions.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_cloudrun_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "run.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_gke_api" {
-//  depends_on = [google_project.project, google_project_service.enable_compute_api]
-//  project = google_project.project.id
-//  service = "container.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
-//resource "google_project_service" "enable_binaryauthorization_api" {
-//  depends_on = [google_project.project]
-//  project = google_project.project.id
-//  service = "binaryauthorization.googleapis.com"
-//  disable_on_destroy = false
-//}
-//
+resource "google_project_service" "enable_bigtable_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "bigtable.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_bigquery_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "bigquery.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_logging_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "logging.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_cloudkms_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "cloudkms.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_storage_component_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "storage-component.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_storage_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "storage-api.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_storage_transfer_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "storagetransfer.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_dataflow_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "dataflow.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_cloudbuild_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_cloudfunctions_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "cloudfunctions.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_cloudrun_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_gke_api" {
+  depends_on = [google_project.project, google_project_service.enable_compute_api]
+  project = google_project.project.id
+  service = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "enable_binaryauthorization_api" {
+  depends_on = [google_project.project]
+  project = google_project.project.id
+  service = "binaryauthorization.googleapis.com"
+  disable_on_destroy = false
+}
+
