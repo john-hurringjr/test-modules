c4b0d5789299810b737c18dade32684daefb300c
John Hurring Jr <jhurring@google.com>
2019-12-03 13:08:24 -0500

    Changed Billing Admin permission to Billing Creator

--- org-iam/main.tf
+++ org-iam/main.tf
@@ -165,7 +165,7 @@ data "google_iam_policy" "org_node_iam_policy_data" {
   }
 
   binding {
-    role = "roles/billing.admin"
+    role = "roles/billing.creator"
     members = [
       "group:${var.billing_admins_group}",
     ]
