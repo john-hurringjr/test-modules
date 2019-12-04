aa80277ffb6d8d87fe70d06fed918d76049f6be5
John Hurring Jr <jhurring@google.com>
2019-12-04 13:42:53 -0500

    Added org iam threat detection

--- org-iam/main.tf
+++ org-iam/main.tf
@@ -171,6 +171,13 @@ data "google_iam_policy" "org_node_iam_policy_data" {
     ]
   }
 
+  binding {
+    role = "roles/threatdetection.editor"
+    members = [
+      "serviceAccount:${var.terraform_service_account}", "group:${var.security_admins_group}"
+    ]
+  }
+
 //  binding {
 //    role = "roles/organizationViewer"
 //    members = [
