4c0859bf3b99aaad0af49af1561c1715b8ba5c1d
John Hurring Jr <jhurring@google.com>
2020-04-10 17:47:40 -0400

    Continuing updates to prep for multi org setup

--- gcs/iac-build-bootstrap/gcs.tf
+++ gcs/iac-build-bootstrap/gcs.tf
@@ -33,116 +33,116 @@ resource "google_storage_bucket" "gcs_bucket_for_backend_var_files" {
 
 resource "google_storage_bucket_object" "org_node_stuff_vars" {
   bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "01-org-node-stuff/blankforfolder"
+  name    = "01-org-node-stuff/"
   content = "nada"
 }
 
 resource "google_storage_bucket_object" "folder_structure_and_policies_vars" {
   bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "/02-folder-structure-and-policies/blankforfolder"
+  name    = "02-folder-structure-and-policies/"
   content = "nada2"
 }
+//
+//resource "google_storage_bucket_object" "shared_services_projects_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "03-shared-services-projects/"
+//  content = "object/"
+//}
+//
+//resource "google_storage_bucket_object" "shared_services_resources_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "04-shared-services-resources/"
+//  content = "/object/"
+//}
+//
+//resource "google_storage_bucket_object" "prod_projects_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "/05-prod-projects/test/"
+//  content = "object"
+//}
+//
+//resource "google_storage_bucket_object" "non_prod_projects_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "06-non-prod-projects/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "prod_resources_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "07-prod-resources/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "non_prod_resources_vars" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
+//  name    = "08-non-prod-resources/"
+//  content = " "
+//}
 
-resource "google_storage_bucket_object" "shared_services_projects_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "03-shared-services-projects/"
-  content = "object/"
-}
-
-resource "google_storage_bucket_object" "shared_services_resources_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "04-shared-services-resources/"
-  content = "/object/"
-}
-
-resource "google_storage_bucket_object" "prod_projects_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "/05-prod-projects/test/"
-  content = "object"
-}
-
-resource "google_storage_bucket_object" "non_prod_projects_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "06-non-prod-projects/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "prod_resources_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "07-prod-resources/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "non_prod_resources_vars" {
-  bucket  = google_storage_bucket.gcs_bucket_for_backend_var_files.name
-  name    = "08-non-prod-resources/"
-  content = " "
-}
 
 
-
-/******************************************
-  State Files Bucket
- *****************************************/
-
-# This bucket will host your backend.tf files and auto.tfvars files
-resource "google_storage_bucket" "gcs_bucket_for_state_files" {
-  name                = var.gcs_backend_state_files_unique_id
-  project             = var.project_id
-  location            = "US"
-  storage_class       = "MULTI_REGIONAL"
-  bucket_policy_only  = true
-
-  versioning {
-    enabled = true
-  }
-
-}
-
-resource "google_storage_bucket_object" "org_node_stuff_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "01-org-node-stuff/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "folder_structure_and_policies_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "02-folder-structure-and-policies/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "shared_services_projects_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "03-shared-services-projects/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "shared_services_resources_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "04-shared-services-resources/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "prod_projects_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "05-prod-projects/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "non_prod_projects_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "06-non-prod-projects"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "prod_resources_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "07-prod-resources/"
-  content = " "
-}
-
-resource "google_storage_bucket_object" "non_prod_resources_state" {
-  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
-  name    = "08-non-prod-resources/"
-  content = " "
-}
+///******************************************
+//  State Files Bucket
+// *****************************************/
+//
+//# This bucket will host your backend.tf files and auto.tfvars files
+//resource "google_storage_bucket" "gcs_bucket_for_state_files" {
+//  name                = var.gcs_backend_state_files_unique_id
+//  project             = var.project_id
+//  location            = "US"
+//  storage_class       = "MULTI_REGIONAL"
+//  bucket_policy_only  = true
+//
+//  versioning {
+//    enabled = true
+//  }
+//
+//}
+//
+//resource "google_storage_bucket_object" "org_node_stuff_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "01-org-node-stuff/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "folder_structure_and_policies_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "02-folder-structure-and-policies/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "shared_services_projects_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "03-shared-services-projects/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "shared_services_resources_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "04-shared-services-resources/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "prod_projects_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "05-prod-projects/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "non_prod_projects_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "06-non-prod-projects"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "prod_resources_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "07-prod-resources/"
+//  content = " "
+//}
+//
+//resource "google_storage_bucket_object" "non_prod_resources_state" {
+//  bucket  = google_storage_bucket.gcs_bucket_for_state_files.name
+//  name    = "08-non-prod-resources/"
+//  content = " "
+//}
