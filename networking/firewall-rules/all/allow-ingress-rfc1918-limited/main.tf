731e6affd5786eb24042b7bdd16f50a9e136c35f
John Hurring Jr <jhurring@google.com>
2020-05-04 10:12:07 -0400

    Added in new firewall rules

---
+++ networking/firewall-rules/all/allow-ingress-rfc1918-limited/main.tf
@@ -0,0 +1,36 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Firewall Rule
+ *****************************************/
+
+resource "google_compute_firewall" "firewall_rule" {
+  provider        = google-beta
+  project         = var.project_id
+  name            = "${var.network_name}-allow-all-rfc-1918-ingress-limited"
+  network         = var.network_self_link
+  description     = "Allows ingress of SSH, RDP, HTTPS from all rfc1918 ranges."
+  direction       = "INGRESS"
+  enable_logging  = var.enable_logging
+  priority        = var.priority
+  source_ranges = ["10.0.0.0/8", "192.168.0.0/24", "172.16.0.0/16"]
+
+  allow {
+    protocol = "tcp"
+    ports = ["22", "3389", "443"]
+  }
+
+}
