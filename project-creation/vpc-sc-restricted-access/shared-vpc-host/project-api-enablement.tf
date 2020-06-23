155c62b8a0ffd67b1ee8f7747e3248d1bca21fa3
John Hurring Jr <jhurring@google.com>
2020-06-23 13:51:49 -0400

    Added network management to api enablement for host vpc projects

--- project-creation/vpc-sc-restricted-access/shared-vpc-host/project-api-enablement.tf
+++ project-creation/vpc-sc-restricted-access/shared-vpc-host/project-api-enablement.tf
@@ -59,3 +59,10 @@ resource "google_project_service" "enable_gke_api" {
   disable_on_destroy  = false
 }
 
+resource "google_project_service" "enable_network_management_api" {
+  depends_on          = [google_project.project, google_project_service.enable_compute_api]
+  project             = google_project.project.project_id
+  service             = "networkmanagement.googleapis.com"
+  disable_on_destroy  = false
+}
+
