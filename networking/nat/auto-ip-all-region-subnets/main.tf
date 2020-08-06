7b3dfbc0c1b65d58889a0ea8cb0c99cb8ee9971d
John Hurring Jr <jhurring@google.com>
2020-08-05 20:45:03 -0400

    Added the "All" flag to CLoud nAT

--- networking/nat/auto-ip-all-region-subnets/main.tf
+++ networking/nat/auto-ip-all-region-subnets/main.tf
@@ -40,6 +40,6 @@ resource "google_compute_router_nat" "cloud_nat" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   log_config {
     enable = true
-    filter = ""
+    filter = "ALL"
   }
 }
