12c28c8d91b865ec9784adf72878b46d3b5a085d
John Hurring Jr <jhurring@google.com>
2020-08-24 14:52:48 -0400

    Added folder level org policy

---
+++ folder-org-policies/domain-restricted-sharing/variables.tf
@@ -0,0 +1,37 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Varables
+ *****************************************/
+
+variable "domain_identities" {
+  type        = list(string)
+  description = "Root domain identities. Allows all users with identity in domain to see org node. Helps users know they are working within appropriate space"
+}
+
+variable "organization_id" {
+  type        = string
+  description = "Root domain identity. Allows all users with identity in domain to see org node. Helps users know they are working within appropriate space"
+}
+
+variable "inherit_from_parent" {
+  type = bool
+  description = "TRUE or FALSE, to inherit settings from parent."
+}
+
+variable "folder_id" {
+  description = "Folder to apply restriction"
+}
