c27fbf544841b29cb02c0672d7f0ad5884f3034c
John Hurring Jr <jhurring@google.com>
2021-11-17 12:46:56 -0500

    Adding in new bootstrap options

---
+++ cloud-build/iac-build-triggers-and-backend/service-account.tf
@@ -0,0 +1,23 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Create Service Account to run pipeline for 00-trigger
+ *****************************************/
+resource "google_service_account" "cloud_build_service_account" {
+  for_each = var.code_folders
+  account_id  = "cloud-build-sa-${each.value}"
+  project     = var.project_id
+}
