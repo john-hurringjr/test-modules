b0722f38963332ba820901307cef8dbc62b96894
John Hurring Jr <jhurring@google.com>
2021-11-17 13:38:24 -0500

    Adding in new bootstrap options

--- cloud-build/iac-first-build-trigger-and-backend/iam.tf
+++ cloud-build/iac-first-build-trigger-and-backend/iam.tf
@@ -29,6 +29,12 @@ resource "google_project_iam_member" "sa_admin_sa_00_trigger" {
   role   = "roles/iam.serviceAccountAdmin"
 }
 
+resource "google_project_iam_member" "storage_admin_sa_00_trigger" {
+  project = var.project_id
+  member = "serviceAccount:${google_service_account.sa_00_trigger.email}"
+  role   = "roles/storage.admin"
+}
+
 resource "google_storage_bucket_iam_member" "cloud_build_sa_00_trigger_gcs_backend" {
   bucket = google_storage_bucket.first_trigger_backend_state_bucket.id
   member = "serviceAccount:${google_service_account.sa_00_trigger.email}"
